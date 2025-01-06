69abe045a97ba87e7e17131b60aaf240c41e8684
Marco Rieser <marco@rieser.dev>
2025-01-06 15:22:17 +0100

    Remove image proxy action route

--- routes/actions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route as Route;
-use MarcoRieser\StatamicInstagram\ImageProxy;
-
-Route::get('proxy/{hash}.{extension}', ImageProxy::class)->name('statamic-instagram.proxy');
